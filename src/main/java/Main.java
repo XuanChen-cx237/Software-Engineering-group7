191170d7cf412d00618edf291819bd4faaa430c4
XuanChen-cx237 <jp2022213237@qmul.ac.uk>
2025-04-21 00:13:20 +0800

    init project

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+import com.project.gui.MainFrame;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Set the look and feel to system default
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Launch the application
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    MainFrame frame = new MainFrame();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
